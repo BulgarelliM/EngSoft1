0b2d7a5d9da4095361efd14d71d5da4645222dec
Renan Siman <31517127+RenanSiman@users.noreply.github.com>
2021-02-06 23:17:34 -0300

    Changes in DB

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,32 @@
 // init project
 const express = require('express'); // the library we will use to handle requests. import it here
 const app = express(); // instantiate express
-const { MongoClient } = require('mongodb'); // requisicao ao mongo
-const uri = 'mongodb+srv://admin-tp1:tp1-engsoftware@cluster0.af72t.mongodb.net/test?authSource=admin&replicaSet=atlas-9h1mtz-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-
-const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-async function run() {
-    try {
-        await client.connect();
-        const database = client.db('DB-imoveis');
-        const collection = database.collection('usuario');
-        // Query for a movie that has the title 'Back to the Future'
-        const query = { title: 'Back to the Future' };
-        const movie = await collection.findOne(query);
-        console.log(movie);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-
-run().catch(console.dir);
+const bancoDeDados = require('./db/bancoDeDados')
+const data = require('./dataTest.json')
+const table = 'endereco'
+const dbName = 'DB-imoveis'
 
 app.use(require("cors")()) // allow Cross-domain requests 
 app.use(require('body-parser').json()) // When someone sends something to the server, we can recieve it in JSON format
 
 // base route. Responds to GET requests to the root route ('/')
-app.get("/", (req, res) => {
-    res.send("Home sweet home 🏚") // always responds with the string "TODO"
+app.get(`/${table}`, (req, res) => {
+    res.send(bancoDeDados.run()) // always responds with the string "TODO"
 });
 
 // base route. Responds to POST requests to the root route
-app.post("/", (req, res) => {
-    res.send("Sending it through the post 📬") // always responds with the string "TODO"
+app.post(`/${table}`, (req, res) => {
+    const insert = bancoDeDados.insert(dbName, table, data) // always responds with the string "TODO"
+    res.send(insert)
 });
 
 // Responds to PUT requests to the root route
-app.put("/", (req, res) => {
+app.put(`/${table}`, (req, res) => {
     res.send("Don't you dare put me up to this.") // always responds with the string "TODO"
+});
+
+app.delete(`/${table}`, (req, res) => {
+    res.send("Delete something.") // always responds with the string "TODO"
 });
 
 
